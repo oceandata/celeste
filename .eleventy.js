8da789f1d5a6abf5377e78f96b37c5656e03e8e0
Aleksei Saenko <saenko.alexei@gmail.com>
2019-11-23 20:12:22 +0200

    feat: add page with examples
    - render content of examples
    - add syntaxhighlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+module.exports = function(config) {
+  config.addPassthroughCopy('src/assets/images');
+
+  config.addPlugin(syntaxHighlight);
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
---
+++ src/examples/2/index.js
@@ -0,0 +1,23 @@
+// We’ll add a tile layer to add to our map, in this case it’s a OSM tile layer.
+// Creating a tile layer usually involves setting the URL template for the tile images
+const osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+const osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const osm = L.tileLayer(osmUrl, {
+  maxZoom: 18,
+  attribution: osmAttrib
+});
+
+const wmsLayer = L.tileLayer.wms(
+  'http://nrt.cmems-du.eu/thredds/wms/global-analysis-forecast-bio-001-028-daily',
+  {
+    layers: 'chl',
+    format: 'image/png',
+    transparent: true,
+  }
+);
+
+// initialize the map on the "map" div with a given center and zoom
+const map = L.map('map').setView([59.423885, 24.700530], 5)
+  .addLayer(osm)
+  .addLayer(wmsLayer);
