b774f7015e89e432887b3e1babc0217619552349
Aleksei Saenko <saenko.alexei@gmail.com>
2019-11-24 11:48:35 +0200

    feat: add styles to example pages, show embeds

--- examples/global_ocean_biogeochemistry_analysis_and_forecast/script.js
+++ examples/global_ocean_biogeochemistry_analysis_and_forecast/script.js
@@ -1,22 +1,22 @@
 // We’ll add a tile layer to add to our map, in this case it’s a OSM tile layer.
 // Creating a tile layer usually involves setting the URL template for the tile images
-var osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  osm = L.tileLayer(osmUrl, {
-    maxZoom: 18,
-    attribution: osmAttrib
-  });
-
-//var stamen = new L.StamenTileLayer("toner-lite");
-
-var wmsLayer = L.tileLayer.wms('http://nrt.cmems-du.eu/thredds/wms/global-analysis-forecast-bio-001-028-daily', {
-  layers: 'chl',
-  format: 'image/png',
-  transparent: true,
+const osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+const osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const osm = L.tileLayer(osmUrl, {
+  maxZoom: 18,
+  attribution: osmAttrib
 });
+
+const wmsLayer = L.tileLayer.wms(
+  'http://nrt.cmems-du.eu/thredds/wms/global-analysis-forecast-bio-001-028-daily',
+  {
+    layers: 'chl',
+    format: 'image/png',
+    transparent: true,
+  }
+);
 
 // initialize the map on the "map" div with a given center and zoom
 var map = L.map('map').setView([59.423885, 24.700530], 5)
-  //.addLayer(stamen)
   .addLayer(osm)
-	 	  .addLayer(wmsLayer);
+  .addLayer(wmsLayer);
--- website/src/_data/examples.js
+++ website/src/_data/examples.js
@@ -5,13 +5,16 @@ const examplesDir = '../../../examples';
 
 const dirList = fs.readdirSync(path.join(__dirname, examplesDir));
 
-const examples = dirList.map((item) => ({
-  id: item,
-  title: require(path.join(__dirname, examplesDir, item, 'meta.json')).title,
-  jsContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'script.js'), 'utf8'),
-  cssContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'styles.css'), 'utf8'),
-  htmlContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'layout.html'), 'utf8'),
-}));
+const examples = dirList.map((item) => {
+  const data = require(path.join(__dirname, examplesDir, item, 'meta.json'));
+  return {
+    ...data,
+    id: item,
+    jsContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'script.js'), 'utf8'),
+    cssContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'styles.css'), 'utf8'),
+    htmlContent: fs.readFileSync(path.join(__dirname, examplesDir, item, 'layout.html'), 'utf8'),
+  }
+});
 
 module.exports = {
   list: examples,
